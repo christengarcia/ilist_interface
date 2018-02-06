3dc5e85648e2fb476ebd99f8a523145d6d94dd06
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-07 09:36:42 +1100

    Initial commit

---
+++ ilist_interface/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ *  C# Program to Demonstrate iList Interface
+ */
+using System;
+using System.Collections.Generic;
+
+namespace ilist_interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[3];
+            a[0] = 1;
+            a[1] = 2;
+            a[2] = 3;
+            Display(a);
+
+            List<int> list = new List<int>();
+            list.Add(5);
+            list.Add(7);
+            list.Add(9);
+            Display(list);
+            Console.ReadLine();
+        }
+
+
+    }
+}
